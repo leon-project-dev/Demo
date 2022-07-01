b4c329df81d92463cb27fd02dffa60299c23b956
leon-project-dev <81818946+leon-project-dev@users.noreply.github.com>
2022-07-01 10:50:03 +0800

    1

--- publish.py
+++ publish.py
@@ -45,19 +45,25 @@ def DeleteOldFolder(path: str):
 
     shutil.rmtree(os.path.join(path, dirs[-1]))
 
+# 保存版本
+def SaveVersion(content: str):
+    fs = open("c:/pub/version.txt", "w+")
+    fs.write(content)
+    fs.close()
+
 
 # 文件夹命名规则： 日期-版本号  版本号：1.1.1.1
 # 返回前面是大版本，后面是小版本
 def GetVersion(folderName: str):
     names = folderName.split('-')
     if len(names) != 2:
-        raise RuntimeError(f"文件夹命名不合法, folder: {folderName}")
+        raise RuntimeError(f'folder name illegal, folder: {folderName}')
     
     verStr = names[1]
     # 检测version的合法性
     vers = verStr.split('.')
     if len(vers) != 4:
-        raise RuntimeError(f"文件夹命名不合法, folder: {folderName}" )
+        raise RuntimeError(f'folder name illegal, folder: {folderName}' )
 
     return f'{vers[0]}.{vers[1]}.{vers[2]}', vers[3]    
 
@@ -72,22 +78,28 @@ if __name__ == "__main__":
     dirTotal = int(sys.argv[2])
 
     if not os.path.exists(checkDir):
-        raise ArgumentError("需要检测的文件夹不存在, path: " + checkDir)
-    
-    # 检查当前文件夹的文件夹个数
-    if GetFileFolderCount(checkDir) > dirTotal:
-        DeleteOldFolder(checkDir)           # 刪除最老的文件夾        
-    
+        print(f'{checkDir} not exist, is frist publish')
+        SaveVersion("")                                # 当前文件夹目录不存在，说明第一次发布
+    else:
 
-    # 获取最新的版本号 保存到文件夹 
-    dirName = GetNewFolderName(checkDir)
-    # 解析文件夹名字返回版本号 ? 文件夹的命名方式
-    mainVesion, revisionVersion = GetVersion(dirName)
+        folders = GetFileFolderCount(checkDir)
+        if folders == 0:                # 当前夹子没有版本文件，为第一次发布
+            print(f'{checkDir} exist, but no child dir, is frist publish')
+            SaveVersion("")
+        else:
 
-    # 将主版本和修订版本写到文件中
-    fo = open("c:/pub/version.txt", "w+")
-    fo.write( f'{mainVesion},{revisionVersion}')
-    print("!!!执行完成!!!")
+            # 检查当前文件夹的文件夹个数
+            if folders > dirTotal:
+                DeleteOldFolder(checkDir)           # 刪除最老的文件夾        
+    
+            # 获取最新的版本号 保存到文件夹 
+            dirName = GetNewFolderName(checkDir)
+            # 解析文件夹名字返回版本号 ? 文件夹的命名方式
+            mainVesion, revisionVersion = GetVersion(dirName)
+
+            # 将主版本和修订版本写到文件中
+            SaveVersion(f'{mainVesion},{revisionVersion}')    
+            print("!!!run success!!!")
 
 
 
